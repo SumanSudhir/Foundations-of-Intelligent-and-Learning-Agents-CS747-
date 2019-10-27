a214af460ff57deb7e9b75295a42c93ddbd4f56f
SumanSudhir <sudhiriitb27@gmail.com>
2019-10-27 10:52:08 +0530

    assign3 code done

--- cs747-pa3/src/algorithm.py
+++ cs747-pa3/src/algorithm.py
@@ -19,20 +19,27 @@ def value_function(dataFileName):
     with open(dataFileName) as f:
         for i, l in enumerate(f):
             pass
-    num_lines = i - 3
+    episode = i - 3
+
     number_of_states = int(data_file.readline())
     number_of_actions = int(data_file.readline())
     discount_factor = float(data_file.readline())
 
-    for i in range(num_lines):
+    for i in range(episode):
         read_line = data_file.readline().split()
-        states.append(read_line[0])
-        action.append(read_line[1])
-        reward.append(read_line[2])
+        states.append(int(read_line[0]))
+        action.append(int(read_line[1]))
+        reward.append(float(read_line[2]))
 
     n_plus_state = data_file.readline()
-    
-    # print(num_lines)
+    states.append(int(n_plus_state))
+    alpha = 100.0/episode
+    value = [0]*number_of_states
+    m = 0
+    for i in range(episode):
+        value[states[i]] += alpha*(reward[i] + discount_factor*value[states[i+1]] - value[states[i]])
+
+    print(value)
     # print(len(states))
     # print(n_plus_state)
 
